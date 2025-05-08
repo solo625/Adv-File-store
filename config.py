10b6d3e8470f384bc7966f0ca13886226ddefcc6
solo625 <wrio202365@gmail.com>
2025-05-08 23:00:14 +0530

    Update config.py

--- config.py
+++ config.py
@@ -75,7 +75,7 @@ CMD_TXT = """<blockquote><b>» 𝗔𝗗𝗠𝗜𝗡𝗦 𝗖𝗢𝗠𝗠𝗔𝗡
 <b>›› /count :</b> ᴄᴏᴜɴᴛ</blockquote>
 """
 #--------------------------------------------
-CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", "<b>• ʙʏ @otakusolo</b>") #set your Custom Caption here, Keep None for Disable Custom Caption
+CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", "") #set your Custom Caption here, Keep None for Disable Custom Caption
 PROTECT_CONTENT = True if os.environ.get('PROTECT_CONTENT', "False") == "True" else False #set True if you want to prevent users from forwarding files from bot
 #--------------------------------------------
 #Set true if you want Disable your Channel Posts Share button
